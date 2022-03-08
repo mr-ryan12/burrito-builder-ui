7b929272d4d9eac1d1176c5440e4f1cdfac3e459
Ryan McBride <mr.ryan444@gmail.com>
2022-03-08 10:40:31 -0700

    Add and pass user flow tests

--- cypress/integration/burritoBuilder_spec.js
+++ cypress/integration/burritoBuilder_spec.js
@@ -4,8 +4,28 @@ describe('Burrito Builder User Flow', () => {
       .visit('http://localhost:3000')
   });
 
+  // it('Shoud have a background image', () => {
+  //   cy.get('main')
+  //     .should('have.css', 'background-image', "url('../../burritos.jpg')")
+  // })
+
   it('Should have a heading', () => {
     cy.get('h1')
       .should('have.text', 'Burrito Builder')
+  });
+
+  it('Should have an order form', () => {
+    cy.get('form')
+      .should('exist')
+  });
+
+  it('Should have an orders container', () => {
+    cy.get('.orders-container')
+      .should('exist')
+  });
+
+  it('Should have 3 orders in the container', () => {
+    cy.get('.order')
+      .should('have.length', 3)
   });
 });
